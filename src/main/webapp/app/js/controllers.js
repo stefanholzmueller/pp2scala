c8cab564af2c8004cbf3bba46032e6f4e64a830e
Stefan Holzmüller <stefanholzmueller@github>
2013-01-18 06:54:42 +0100

    added AngularJS and its basic form example

---
+++ src/main/webapp/app/js/controllers.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/* Controllers */
+
+
+function FormController($scope) {
+    var user = $scope.user = {
+      name: 'John Smith',
+      address:{line1: '123 Main St.', city:'Anytown', state:'AA', zip:'12345'},
+      contacts:[{type:'phone', value:'1(234) 555-1212'}]
+    };
+    $scope.state = /^\w\w$/;
+    $scope.zip = /^\d\d\d\d\d$/;
+   
+    $scope.addContact = function() {
+       user.contacts.push({type:'email', value:''});
+    };
+   
+    $scope.removeContact = function(contact) {
+      for (var i = 0, ii = user.contacts.length; i < ii; i++) {
+        if (contact === user.contacts[i]) {
+          $scope.user.contacts.splice(i, 1);
+        }
+      }
+    };
+  }
