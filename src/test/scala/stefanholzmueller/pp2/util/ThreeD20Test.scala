95613b8f0c20a676245fc1038f65bbc626c22dba
Stefan Holzmüller <stefanholzmueller@github>
2013-03-31 17:12:08 +0200

    TestNG in Scala via mvn

---
+++ src/test/scala/stefanholzmueller/pp2/util/ThreeD20Test.scala
@@ -0,0 +1,13 @@
+package stefanholzmueller.pp2.util
+
+import org.testng.annotations.Test
+import org.hamcrest.MatcherAssert._
+import org.hamcrest.Matchers._
+
+class ThreeD20Test {
+
+  @Test def test {
+    assertThat(true, is(false))
+  }
+  
+}
