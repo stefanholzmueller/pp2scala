67542c58921049cbb1b96a171c3d47ea3e657ece
Stefan Holzmüller <stefanholzmueller@github>
2013-03-31 12:21:37 +0200

    formatted file

--- src/main/webapp/ui/js/controllers.js
+++ src/main/webapp/ui/js/controllers.js
@@ -3,35 +3,35 @@
 /* Controllers */
 
 function clone(obj) {
-    return JSON.parse(JSON.stringify(obj));
+	return JSON.parse(JSON.stringify(obj));
 }
 
 function CheckController($scope, $http) {
-    var check = $scope.check = {
-	attribute1 : 12,
-	attribute2 : 12,
-	attribute3 : 12,
-	value : 4,
-	difficulty : 0,
-	minimumQuality : true
-    };
-    
-    $scope.recalculate = function() {
-	$http.put('../rest/check/statistics', check).success(function(result) {
-	    result.check = clone(check);
-	    $scope.result = result;
-	}).error(function(data, status) {
-	    alert("status=" + status);
-	});
-    };
+	var check = $scope.check = {
+		attribute1 : 12,
+		attribute2 : 12,
+		attribute3 : 12,
+		value : 4,
+		difficulty : 0,
+		minimumQuality : true
+	};
+
+	$scope.recalculate = function() {
+		$http.put('../rest/check/statistics', check).success(function(result) {
+			result.check = clone(check);
+			$scope.result = result;
+		}).error(function(data, status) {
+			alert("status=" + status);
+		});
+	};
 }
 
 function RollController($scope, $http) {
-    $scope.roll = function(pips) {
-	$http.get('../rest/random/d' + pips).success(function(data) {
-	    $scope.result = data;
-	}).error(function(data, status) {
-	    alert("status=" + status);
-	});
-    }
+	$scope.roll = function(pips) {
+		$http.get('../rest/random/d' + pips).success(function(data) {
+			$scope.result = data;
+		}).error(function(data, status) {
+			alert("status=" + status);
+		});
+	}
 }
