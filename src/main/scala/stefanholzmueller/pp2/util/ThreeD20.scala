29301a1a76bd091595b885f303aa73cb36933cbb
Stefan Holzmüller <stefanholzmueller@github>
2013-04-01 15:14:59 +0200

    added a validation message

--- src/main/scala/stefanholzmueller/pp2/util/ThreeD20.scala
+++ src/main/scala/stefanholzmueller/pp2/util/ThreeD20.scala
@@ -3,7 +3,7 @@ package stefanholzmueller.pp2.util
 class ThreeD20(a: Int, b: Int, c: Int) { // TODO Dice20(varargs)
 	val elements = Vector(a, b, c)
 	for (x <- elements) {
-		require(x >= 1 && x <= 20)
+		require(x >= 1 && x <= 20, "dice values must be between 1 and 20")
 	}
 
 	def apply(i: Int) = elements(i)
