c91a562b298b1da5f9e1e0cfc47313469e160c7d
Stefan Holzmüller <stefanholzmueller@github>
2013-04-01 10:52:06 +0200

    added validation; formatting

--- src/main/scala/stefanholzmueller/pp2/util/ThreeD20.scala
+++ src/main/scala/stefanholzmueller/pp2/util/ThreeD20.scala
@@ -1,13 +1,16 @@
 package stefanholzmueller.pp2.util
 
 class ThreeD20(a: Int, b: Int, c: Int) {
-  val elements = Vector(a, b, c)
+	val elements = Vector(a, b, c)
+	for (x <- elements) {
+		require(x >= 1 && x <= 20)
+	}
 
-  def apply(i: Int) = elements(i)
+	def apply(i: Int) = elements(i)
 
-  def allEqualTo(n: Int) = elements.forall(_ == n)
+	def allEqualTo(n: Int) = elements.forall(_ == n)
 
-  def twoEqualTo(n: Int) = elements.filter(_ == n).length >= 2
+	def twoEqualTo(n: Int) = elements.filter(_ == n).length >= 2
 
-  def twoSameValues() = elements(0) == elements(1) || elements(0) == elements(2) || elements(1) == elements(2)
+	def twoSameValues() = elements(0) == elements(1) || elements(0) == elements(2) || elements(1) == elements(2)
 }
--- src/test/scala/stefanholzmueller/pp2/util/ThreeD20Test.scala
+++ src/test/scala/stefanholzmueller/pp2/util/ThreeD20Test.scala
@@ -6,35 +6,50 @@ import org.hamcrest.Matchers._
 
 class ThreeD20Test {
 
-  @Test
-  def applyReturnsDice {
-    val dice = new ThreeD20(1, 2, 3)
+	@Test(expectedExceptions = Array(classOf[IllegalArgumentException]))
+	def requireSaneValues1 {
+		new ThreeD20(1, 2, -3)
+	}
 
-    assertThat(dice(0), is(1))
-    assertThat(dice(1), is(2))
-    assertThat(dice(2), is(3))
-  }
+	@Test(expectedExceptions = Array(classOf[IllegalArgumentException]))
+	def requireSaneValues2 {
+		new ThreeD20(111, 2, 3)
+	}
 
-  @Test
-  def allEqualTo {
-    assertThat(new ThreeD20(3, 3, 3).allEqualTo(1), is(false))
-    assertThat(new ThreeD20(1, 2, 3).allEqualTo(1), is(false))
-    assertThat(new ThreeD20(1, 1, 1).allEqualTo(1), is(true))
-  }
+	@Test(expectedExceptions = Array(classOf[IllegalArgumentException]))
+	def requireSaneValues3 {
+		new ThreeD20(0, Int.MaxValue, Int.MinValue)
+	}
 
-  @Test
-  def twoEqualTo {
-    assertThat(new ThreeD20(3, 3, 3).twoEqualTo(1), is(false))
-    assertThat(new ThreeD20(1, 2, 3).twoEqualTo(1), is(false))
-    assertThat(new ThreeD20(1, 1, 2).twoEqualTo(1), is(true))
-    assertThat(new ThreeD20(1, 1, 1).twoEqualTo(1), is(true))
-  }
+	@Test
+	def applyReturnsDice {
+		val dice = new ThreeD20(1, 2, 3)
 
-  @Test
-  def twoSameValues {
-    assertThat(new ThreeD20(1, 2, 3).twoSameValues(), is(false))
-    assertThat(new ThreeD20(1, 1, 2).twoSameValues(), is(true))
-    assertThat(new ThreeD20(1, 1, 1).twoSameValues(), is(true))
-  }
+		assertThat(dice(0), is(1))
+		assertThat(dice(1), is(2))
+		assertThat(dice(2), is(3))
+	}
+
+	@Test
+	def allEqualTo {
+		assertThat(new ThreeD20(3, 3, 3).allEqualTo(1), is(false))
+		assertThat(new ThreeD20(1, 2, 3).allEqualTo(1), is(false))
+		assertThat(new ThreeD20(1, 1, 1).allEqualTo(1), is(true))
+	}
+
+	@Test
+	def twoEqualTo {
+		assertThat(new ThreeD20(3, 3, 3).twoEqualTo(1), is(false))
+		assertThat(new ThreeD20(1, 2, 3).twoEqualTo(1), is(false))
+		assertThat(new ThreeD20(1, 1, 2).twoEqualTo(1), is(true))
+		assertThat(new ThreeD20(1, 1, 1).twoEqualTo(1), is(true))
+	}
+
+	@Test
+	def twoSameValues {
+		assertThat(new ThreeD20(1, 2, 3).twoSameValues(), is(false))
+		assertThat(new ThreeD20(1, 1, 2).twoSameValues(), is(true))
+		assertThat(new ThreeD20(1, 1, 1).twoSameValues(), is(true))
+	}
 
 }
