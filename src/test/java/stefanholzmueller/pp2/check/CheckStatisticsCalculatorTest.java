2d342b849e400ace1afad4532e99aea6fff7671d
Stefan Holzmüller <stefanholzmueller@github>
2013-04-19 11:36:57 +0200

    renamed test files

--- src/test/java/stefanholzmueller/pp2/check/CheckStatisticsCalculatorTest.java
+++ src/test/java/stefanholzmueller/pp2/check/CheckStatisticsCalculatorTest.java
@@ -13,71 +13,72 @@ import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import stefanholzmueller.pp2.check.Check;
-import stefanholzmueller.pp2.check.CheckResultCalculator;
-import stefanholzmueller.pp2.check.CheckOutcome;
-import stefanholzmueller.pp2.check.CheckResult;
-import stefanholzmueller.pp2.check.CheckStatistics;
-import stefanholzmueller.pp2.check.CheckStatisticsCalculator;
 import stefanholzmueller.pp2.util.IntTriple;
-
 
 public class CheckStatisticsCalculatorTest {
 
-    private StatisticsGatherer calculator;
-    private Check trivialCheck;
+	private StatisticsGatherer gatherer;
+	private Check trivialCheck;
 
-    @Mock
-    private CheckResultCalculator checkDecider;
+	@Mock
+	private CheckResultCalculator checkExaminer;
 
-    @BeforeMethod
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
+	@BeforeMethod
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
 
-        calculator = new CheckStatisticsCalculator(checkDecider);
-        trivialCheck = new Check(12, 12, 12, 4);
-    }
+		gatherer = new CheckStatisticsCalculator(checkExaminer);
+		trivialCheck = new Check(12, 12, 12, 4);
+	}
 
-    @Test
-    public void shouldRunCheckDecider8000Times() throws Exception {
-        allChecksAreSuccessful();
+	@Test
+	public void shouldRunCheckDecider8000Times() throws Exception {
+		allChecksAreSuccessful();
 
-        calculator.gather(trivialCheck);
+		gatherer.gather(trivialCheck);
 
-        verify(checkDecider, times(8000)).examine(eq(trivialCheck), any(IntTriple.class));
-    }
+		verify(checkExaminer, times(8000)).examine(eq(trivialCheck),
+				any(IntTriple.class));
+	}
 
-    @Test
-    public void shouldReturnMaximumProbabilityIfEveryCheckIsSuccessful() throws Exception {
-        allChecksAreSuccessful();
+	@Test
+	public void shouldReturnMaximumProbabilityIfEveryCheckIsSuccessful()
+			throws Exception {
+		allChecksAreSuccessful();
 
-        CheckStatistics checkStatistics = calculator.gather(trivialCheck);
+		CheckStatistics checkStatistics = gatherer.gather(trivialCheck);
 
-        assertThat(checkStatistics.getProbabilityOfSuccess(), is(1.0));
-    }
+		assertThat(checkStatistics.getProbabilityOfSuccess(), is(1.0));
+	}
 
-    @Test
-    public void shouldReturnAverageQualityIfEveryCheckIsSuccessful() throws Exception {
-        allChecksAreSuccessful();
+	@Test
+	public void shouldReturnAverageQualityIfEveryCheckIsSuccessful()
+			throws Exception {
+		allChecksAreSuccessful();
 
-        CheckStatistics checkStatistics = calculator.gather(trivialCheck);
+		CheckStatistics checkStatistics = gatherer.gather(trivialCheck);
 
-        assertThat(checkStatistics.getAverageQuality(), is(4.0));
-    }
+		assertThat(checkStatistics.getAverageQuality(), is(4.0));
+	}
 
-    @Test
-    public void shouldReturnAverageQualityForSuccesses() throws Exception {
-        CheckResult successful = new CheckResult(CheckOutcome.SUCCESSFUL, 3, null);
-        CheckResult unsuccessful = new CheckResult(CheckOutcome.UNSUCCESSFUL, null, null);
-        when(checkDecider.examine(eq(trivialCheck), any(IntTriple.class))).thenReturn(successful, unsuccessful);
+	@Test
+	public void shouldReturnAverageQualityForSuccesses() throws Exception {
+		CheckResult successful = new CheckResult(CheckOutcome.SUCCESSFUL, 3,
+				null);
+		CheckResult unsuccessful = new CheckResult(CheckOutcome.UNSUCCESSFUL,
+				null, null);
+		when(checkExaminer.examine(eq(trivialCheck), any(IntTriple.class)))
+				.thenReturn(successful, unsuccessful);
 
-        CheckStatistics checkStatistics = calculator.gather(trivialCheck);
+		CheckStatistics checkStatistics = gatherer.gather(trivialCheck);
 
-        assertThat(checkStatistics.getAverageQualityForSuccesses(), is(3.0));
-    }
+		assertThat(checkStatistics.getAverageQualityForSuccesses(), is(3.0));
+	}
 
-    private void allChecksAreSuccessful() {
-        CheckResult checkResult = new CheckResult(CheckOutcome.SUCCESSFUL, 4, null);
-        when(checkDecider.examine(eq(trivialCheck), any(IntTriple.class))).thenReturn(checkResult);
-    }
+	private void allChecksAreSuccessful() {
+		CheckResult checkResult = new CheckResult(CheckOutcome.SUCCESSFUL, 4,
+				null);
+		when(checkExaminer.examine(eq(trivialCheck), any(IntTriple.class)))
+				.thenReturn(checkResult);
+	}
 }
