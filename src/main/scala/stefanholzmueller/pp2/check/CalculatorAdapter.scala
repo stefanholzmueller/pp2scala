d572fec4abc96b0fd20dd6f2cee7817c9f56ee9e
Stefan Holzmüller <stefanholzmueller@github>
2013-04-19 11:12:16 +0200

    adapter converts failure gaps

--- src/main/scala/stefanholzmueller/pp2/check/CalculatorAdapter.scala
+++ src/main/scala/stefanholzmueller/pp2/check/CalculatorAdapter.scala
@@ -19,7 +19,7 @@ class CalculatorAdapter extends OutcomeCalculator {
 			case Success(q, g) => new CheckResult(CheckOutcome.SUCCESSFUL, q, g)
 			case AutomaticSuccess(q) => new CheckResult(CheckOutcome.LUCKY_CHECK, q, null)
 			case SpectacularSuccess(q) => new CheckResult(CheckOutcome.SPECTACULAR_SUCCESS, q, null)
-			case Failure(g) => new CheckResult(CheckOutcome.UNSUCCESSFUL, null, g)
+			case Failure(g) => new CheckResult(CheckOutcome.UNSUCCESSFUL, null, -g)
 			case AutomaticFailure() => new CheckResult(CheckOutcome.FUMBLE, null, null)
 			case SpectacularFailure() => new CheckResult(CheckOutcome.SPECTACULAR_FUMBLE, null, null)
 			case Spruchhemmung() => new CheckResult(CheckOutcome.SPRUCHHEMMUNG, null, null)
