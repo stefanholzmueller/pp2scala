eb7bcc198e10b0cc222e5186795cf4c7b239dd9a
Stefan Holzmüller <stefanholzmueller@github>
2013-04-21 13:35:57 +0200

    added more tests

--- src/test/scala/stefanholzmueller/pp2/check/SimpleStatisticsCalculatorTest.scala
+++ src/test/scala/stefanholzmueller/pp2/check/SimpleStatisticsCalculatorTest.scala
@@ -16,4 +16,34 @@ class SimpleStatisticsCalculatorTest {
 		val statistics = SimpleStatisticsCalculator.gather(Options.nothing, List(12, 12, 12), 4, 0)
 		Assert.assertEquals(statistics, Statistics(0.461, 2.6374728850325377))
 	}
+
+	@Test
+	def maximumSuccess {
+		val statistics = SimpleStatisticsCalculator.gather(Options.nothing, List(20, 20, 20), 18, 0)
+		Assert.assertEquals(statistics, Statistics(0.99275, 18))
+	}
+
+	@Test
+	def minimumSuccess {
+		val statistics = SimpleStatisticsCalculator.gather(Options.nothing, List(10, 10, 10), 0, 10)
+		Assert.assertEquals(statistics, Statistics(0.00725, 0))
+	}
+
+	@Test
+	def minimumSuccess_withMinimumQuality {
+		val statistics = SimpleStatisticsCalculator.gather(Options.default, List(10, 10, 10), 0, 10)
+		Assert.assertEquals(statistics, Statistics(0.00725, 1))
+	}
+
+	@Test
+	def minimumSuccess_withOverwhelmingDifficulty {
+		val statistics = SimpleStatisticsCalculator.gather(Options.default, List(12, 12, 12), 4, 20)
+		Assert.assertEquals(statistics, Statistics(0.00725, 4))
+	}
+
+	@Test
+	def maximumSuccess_causedByNegativeDifficulty {
+		val statistics = SimpleStatisticsCalculator.gather(Options.default, List(12, 12, 12), 4, -24)
+		Assert.assertEquals(statistics, Statistics(0.99275, 4))
+	}
 }
