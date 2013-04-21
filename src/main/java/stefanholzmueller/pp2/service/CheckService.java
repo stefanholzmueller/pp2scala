ad7e52cc479e3f5a885b6ae85933144df23985f9
Stefan Holzmüller <stefanholzmueller@github>
2013-04-21 12:06:24 +0200

    injection does not work anymore, hardwired new implementation

--- src/main/java/stefanholzmueller/pp2/service/CheckService.java
+++ src/main/java/stefanholzmueller/pp2/service/CheckService.java
@@ -1,6 +1,5 @@
 package stefanholzmueller.pp2.service;
 
-import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -12,6 +11,7 @@ import stefanholzmueller.pp2.check.CheckResult;
 import stefanholzmueller.pp2.check.CheckRoll;
 import stefanholzmueller.pp2.check.CheckStatistics;
 import stefanholzmueller.pp2.check.OutcomeExaminer;
+import stefanholzmueller.pp2.check.SimpleStatisticsCalculatorAdapter;
 import stefanholzmueller.pp2.check.StatisticsGatherer;
 import stefanholzmueller.pp2.util.IntTriple;
 
@@ -20,26 +20,30 @@ import stefanholzmueller.pp2.util.IntTriple;
 @Produces(MediaType.APPLICATION_JSON)
 public class CheckService {
 
-    private StatisticsGatherer checkStatisticsCalculator;
-    private OutcomeExaminer checkResultCalculator;
+	private StatisticsGatherer statisticsGatherer = new SimpleStatisticsCalculatorAdapter();
+	private OutcomeExaminer checkResultCalculator;
 
-    @Inject
-    public CheckService(StatisticsGatherer checkStatisticsCalculator, OutcomeExaminer checkResultCalculator) {
-        this.checkStatisticsCalculator = checkStatisticsCalculator;
-        this.checkResultCalculator = checkResultCalculator;
-    }
+	public CheckService() {
+	}
 
-    @Path("/statistics")
-    @PUT
-    public CheckStatistics calculateStatistics(Check check) {
-        return checkStatisticsCalculator.gather(check);
-    }
+	// @Inject
+	public CheckService(StatisticsGatherer checkStatisticsCalculator,
+			OutcomeExaminer checkResultCalculator) {
+		this.statisticsGatherer = checkStatisticsCalculator;
+		this.checkResultCalculator = checkResultCalculator;
+	}
 
-    @Path("/result")
-    @PUT
-    public CheckResult calculateResult(CheckRoll checkRoll) {
-        Check check = checkRoll.getCheck();
-        IntTriple dice = checkRoll.getDice();
-        return checkResultCalculator.examine(check, dice);
-    }
+	@Path("/statistics")
+	@PUT
+	public CheckStatistics calculateStatistics(Check check) {
+		return statisticsGatherer.gather(check);
+	}
+
+	@Path("/result")
+	@PUT
+	public CheckResult calculateResult(CheckRoll checkRoll) {
+		Check check = checkRoll.getCheck();
+		IntTriple dice = checkRoll.getDice();
+		return checkResultCalculator.examine(check, dice);
+	}
 }
