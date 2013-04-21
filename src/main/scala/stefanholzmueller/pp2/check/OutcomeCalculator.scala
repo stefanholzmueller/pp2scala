51381bde1446bb36645b232c47063fc00d7e7ebb
Stefan Holzmüller <stefanholzmueller@github>
2013-04-21 19:04:19 +0200

    moved adapters to same source file than the actual implementation

--- src/main/scala/stefanholzmueller/pp2/check/OutcomeCalculator.scala
+++ src/main/scala/stefanholzmueller/pp2/check/OutcomeCalculator.scala
@@ -3,6 +3,30 @@ package stefanholzmueller.pp2.check
 import stefanholzmueller.pp2.util.Dice
 import stefanholzmueller.pp2.util.IntTriple
 
+class OutcomeCalculatorAdapter extends OutcomeExaminer {
+
+	def examine(check: Check, diceTriple: IntTriple): CheckResult = {
+		val options = new Options(check.hasMinimumQuality, check.hasFesteMatrix, check.hasTollpatsch, check.hasSpruchhemmung)
+		val attributes = List(check.getAttribute1, check.getAttribute2, check.getAttribute3)
+		val points = check.getValue
+		val difficulty = check.getDifficulty
+		val dice = new Dice(diceTriple.first, diceTriple.second, diceTriple.third)
+
+		val outcome = OutcomeCalculator.examine(options, attributes, points, difficulty)(dice)
+
+		outcome match {
+			case Success(q, g) => new CheckResult(CheckOutcome.SUCCESSFUL, q, g)
+			case AutomaticSuccess(q) => new CheckResult(CheckOutcome.LUCKY_CHECK, q, null)
+			case SpectacularSuccess(q) => new CheckResult(CheckOutcome.SPECTACULAR_SUCCESS, q, null)
+			case Failure(g) => new CheckResult(CheckOutcome.UNSUCCESSFUL, null, -g) // note the negation
+			case AutomaticFailure() => new CheckResult(CheckOutcome.FUMBLE, null, null)
+			case SpectacularFailure() => new CheckResult(CheckOutcome.SPECTACULAR_FUMBLE, null, null)
+			case Spruchhemmung() => new CheckResult(CheckOutcome.SPRUCHHEMMUNG, null, null)
+		}
+	}
+
+}
+
 object OutcomeCalculator {
 
 	def examine(options: Options, attributes: List[Int], points: Int, difficulty: Int)(dice: Dice): Outcome = {
