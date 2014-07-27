8a3fd44597a801712911009bd57c4e3c26a230d7
Stefan Holzmüller <stefanholzmueller@github>
2014-07-27 16:35:57 +0200

    vertically aligned form

--- src/main/webapp/src/check/controller.ts
+++ src/main/webapp/src/check/controller.ts
@@ -5,9 +5,6 @@ var module = angular.module('pp2.check', []);
 
 module.controller('CheckController', [ '$scope', function ($scope) {
 	var check = $scope.check = {
-		attribute1: 12,
-		attribute2: 12,
-		attribute3: 12,
 		attributes: [12,12,12],
 		value: 4,
 		difficulty: 0,
